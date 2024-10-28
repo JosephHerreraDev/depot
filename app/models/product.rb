552d72a1414b2ca802aff0ebf1ba74c7017e3805
Joseph Herrera <josephherreraleon@gmail.com>
2024-10-28 12:13:27 -0600

    feat: add shopping cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_items
   validates :title, :description, :image_url, presence: true
   validates :title, length: { minimum: 10 }
   validates :title, uniqueness:true 
@@ -6,5 +9,12 @@ class Product < ApplicationRecord
     with: %r{\.(gif|jpg|png|)\z}i,
   message: 'must be a URL for GIF, JPG or PNG image, bruh.'}
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
-
+  private 
+  #esure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_items
+    unless line_items.empty?
+      errors.add(:base, 'Line Items present')
+      throw :abort
+    end
+  end
 end
